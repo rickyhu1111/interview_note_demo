0c0f280e56d5c31f4aca062d027a88b5aa223184
ricky.hu <rickyhu@staff.1111.com.tw>
2025-12-31 14:46:39 +0800

    feat: 🎸 create bottom navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -59,4 +62,7 @@ dependencies {
     implementation(libs.barcode.scanning)
 
     implementation(libs.play.services.code.scanner)
+    
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 }
